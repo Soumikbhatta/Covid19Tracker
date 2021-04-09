193ae7484a3405fc4d8e75540631bb1fd8964889
Soumik Bhatta <soumikbhatta@gmail.com>
2021-04-10 04:53:24 +0530

    added cards

--- src/components/statedata/statedata.js
+++ src/components/statedata/statedata.js
@@ -1,9 +1,11 @@
+/* eslint-disable no-unused-vars */
 import React, { Component } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
 import { Col, Row } from "react-bootstrap";
+import { Card } from "primereact/card";
+import { Toast } from 'primereact/toast';
 
 import District from "../districtwise/districtwise";
-
-
 
 export default class statedata extends Component {
   render() {
@@ -17,43 +19,60 @@ export default class statedata extends Component {
                   <div>
                     <h2 className="text-dark">{this.props.stateData.state}</h2>
                   </div>
-                  <div className="">
+                  <div>
                     <Row className="p-3">
-                      <Col className="text-dark col m-1 items active-cases">
-                        <div className="heading">Activated Cases</div>
-                        <div className="value">
+                      <Col className="text-text col m-1 items confirmed">
+                        <Card
+                          title="Activated Cases"
+                          style={{ width: "15rem", marginBottom: "2em" }}
+                        >
                           {this.props.stateData.active}
-                        </div>
+                        </Card>
                       </Col>
-                      <Col className="text-dark col m-1 items confirm">
-                        <div className="heading">Confirm Cases</div>
-                        <div className="value">
+
+                      <Col className="text-text col m-1 items confirmed">
+                        <Card
+                          title="Confirmed Cases"
+                          style={{ width: "15rem", marginBottom: "2em" }}
+                        >
                           {this.props.stateData.confirmed}
-                        </div>
+                        </Card>
                       </Col>
-                      <Col className="text-dark col m-1 items total-death">
-                        <div className="heading">Total Num Deaths</div>
-                        <div className="value">
+
+                      <Col className="text-text col m-1 items today-deaths">
+                        <Card
+                          title="Total Deaths"
+                          style={{ width: "15rem", marginBottom: "2em" }}
+                        >
                           {this.props.stateData.deaths}
-                        </div>
+                        </Card>
                       </Col>
-                      <Col className="text-dark col m-1 items recovered">
-                        <div className="heading">Total Recovered</div>
-                        <div className="value">
+
+                      <Col className="text-text col m-1 items today-recovered">
+                        <Card
+                          title="Total Recovered"
+                          style={{ width: "15rem", marginBottom: "2em" }}
+                        >
                           {this.props.stateData.recovered}
-                        </div>
+                        </Card>
                       </Col>
+
                       <Col className="text-text col m-1 items today-death">
-                        <div className="heading">Today New Cases</div>
-                        <div className="value">
+                        <Card
+                          title="Today New Cases"
+                          style={{ width: "15rem", marginBottom: "2em" }}
+                        >
                           {this.props.stateData.deltaconfirmed}
-                        </div>
+                        </Card>
                       </Col>
-                      <Col className="text-dark col m-1 items update">
-                        <div className="heading">Last Update Time</div>
-                        <div className="value">
+
+                      <Col className="text-text col m-1 items update">
+                        <Card
+                          title="Last Update Time"
+                          style={{ width: "15rem", marginBottom: "2em" }}
+                        >
                           {this.props.stateData.lastupdatedtime}
-                        </div>
+                        </Card>
                       </Col>
                     </Row>
                   </div>
